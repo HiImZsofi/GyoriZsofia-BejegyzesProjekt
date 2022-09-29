496c8ed3d655fb6f85bca1f5b4be42f26b713e9a
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-29 08:08:40 +0200

    variables and constructor

--- src/hu/petrik/bejegyzes/Bejegyzes.java
+++ src/hu/petrik/bejegyzes/Bejegyzes.java
@@ -1,4 +1,19 @@
 package hu.petrik.bejegyzes;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
 }
