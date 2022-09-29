41946211a51a15c69f91a2837dd1d246cf2f04c5
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-29 08:25:46 +0200

    add static list

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import hu.petrik.bejegyzes.Bejegyzes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static List<Bejegyzes> bejegyzesek = new ArrayList<>();
     public static void main(String[] args) {
-        /*Bejegyzes test = new Bejegyzes("testing", "asdasd");
-        System.out.println(test.toString());*/
+        Bejegyzes test = new Bejegyzes("testing1", "asdasd");
+        Bejegyzes test2 = new Bejegyzes("testing2", "dsadsa");
+        //System.out.println(test.toString());
+        bejegyzesek.add(test);
+        bejegyzesek.add(test2);
     }
 }
