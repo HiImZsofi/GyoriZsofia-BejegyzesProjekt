01330907de0cabe0de099c630271f01cc91a7a54
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-29 08:35:03 +0200

    user can add new posts to list

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import hu.petrik.bejegyzes.Bejegyzes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.System.in;
 
 public class Main {
     static List<Bejegyzes> bejegyzesek = new ArrayList<>();
@@ -11,5 +14,27 @@ public class Main {
         //System.out.println(test.toString());
         bejegyzesek.add(test);
         bejegyzesek.add(test2);
+
+        int postNumber = -1; //-1 hogy belépjen a whileba
+        Scanner sc = new Scanner(in);
+        while(postNumber < 0){
+            System.out.println("Adja meg hány új bejegyzést szeretne készíteni");
+            postNumber = Integer.parseInt(sc.nextLine());
+        }
+        int addedPosts = 0;
+        String newSzerzo;
+        String newTartalom;
+        while(postNumber != addedPosts){
+            addedPosts++;
+            System.out.println("Adja meg a bejegyzés szerzőjét: ");
+            newSzerzo = sc.nextLine();
+            System.out.println("Adja meg a bejegyzés tartalmát: ");
+            newTartalom = sc.nextLine();
+            Bejegyzes userPost = new Bejegyzes(newSzerzo, newTartalom);
+            bejegyzesek.add(userPost);
+        }
+        for (Bejegyzes bejegyzes : bejegyzesek) {
+            System.out.println(bejegyzes);
+        }
     }
 }
