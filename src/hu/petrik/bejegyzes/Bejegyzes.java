500465d14d2c5edbb40c3104d429dad055c0b60e
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-29 08:12:17 +0200

    add getter + setter methods

--- src/hu/petrik/bejegyzes/Bejegyzes.java
+++ src/hu/petrik/bejegyzes/Bejegyzes.java
@@ -16,4 +16,37 @@ public class Bejegyzes {
         this.letrejott = LocalDateTime.now();
         this.szerkesztve = LocalDateTime.now();
     }
+
+    public void setSzerkesztve(LocalDateTime szerkesztve) {
+        this.szerkesztve = szerkesztve;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        setSzerkesztve(LocalDateTime.now());
+    }
+
+    public void like(){
+        this.likeok += 1;
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
 }
